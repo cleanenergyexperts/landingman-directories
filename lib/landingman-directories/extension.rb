59c0483ea1d0f965528a7610557ae465d73cf7cd
Matt Snider <matt@cleanenergyexperts.com>
2016-02-16 06:59:09 -0800

    Allow to work with older versions of middleman

--- lib/landingman-directories/extension.rb
+++ lib/landingman-directories/extension.rb
@@ -7,16 +7,13 @@ module Landingman
     option :js_dir,     'assets/javascripts', 'JavaScripts Directory'
     option :images_dir, 'assets/images',      'Images Directory'
     option :fonts_dir,  'assets/fonts',       'Fonts Directory'
+    option :partials_dir,'partials',          'Partials Directory'
     option :layouts_dir,'layouts',            'Layouts Directory'
 
     def initialize(app, options_hash={}, &block)
       super
       self.configure_paths
     end
-
-    # def after_configuration
-    #   self.configure_paths
-    # end
 
   protected
 
@@ -25,6 +22,16 @@ module Landingman
       app.config[:js_dir]       = options.js_dir
       app.config[:images_dir]   = options.images_dir
       app.config[:fonts_dir]    = options.fonts_dir
+
+      # Use _partials if partials doesn't exist
+      # Only applies to Middleman 3 (partials always come from the root in Middleman 4)
+      if options.partials_dir == 'partials' &&
+        !Dir.exist?(File.join(app.root, 'source', 'partials')) &&
+        Dir.exist?(File.join(app.root, 'source', '_partials')) then
+        app.config[:partials_dir] = '_partials'
+      else
+        app.config[:partials_dir] = options.partials_dir
+      end
 
       # Use _layouts if layouts doesn't exist
       if options.layouts_dir == 'layouts' &&
